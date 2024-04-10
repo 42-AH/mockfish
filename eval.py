7310f2bffefa9666ce9065107d5ce961d452bf07
42-AH <162044943+42-AH@users.noreply.github.com>
2024-04-10 15:20:32 -0600

    Update eval.py

--- eval.py
+++ eval.py
@@ -2,108 +2,86 @@ import chess
 
 #https://www.chessprogramming.org/Simplified_Evaluation_Function
 
-pawntable = [
-    0, 0, 0, 0, 0, 0, 0, 0,
-    50, 50, 50, 50, 50, 50, 50, 50,
-    10, 10, 20, 30, 30, 20, 10, 10,
-    5, 5, 10, 25, 25, 10, 5, 5,
-    0, 0, 0, 20, 20, 0, 0, 0,
-    5, -5, -10, 0, 0, -10, -5, 5,
-    5, 10, 10, -20, -20, 10, 10, 5,
-    0, 0, 0, 0, 0, 0, 0, 0
-    ]
-knightstable = [
-    -50, -40, -30, -30, -30, -30, -40, -50,
-    -40, -20, 0, 0, 0, 0, -20, -40,
-    -30, 0, 10, 15, 15, 10, 0, -30,
-    -30, 5, 15, 20, 20, 15, 5, -30,
-    -30, 0, 15, 20, 20, 15, 0, -30,
-    -30, 5, 10, 15, 15, 10, 5, -30,
-    -40, -20, 0, 5, 5, 0, -20, -40,
-    -50, -40, -30, -30, -30, -30, -40, -50
-    ]
-bishopstable =  [
-    -20, -10, -10, -10, -10, -10, -10, -20,
-    -10, 5, 0, 0, 0, 0, 5, -10,
-    -10, 10, 10, 10, 10, 10, 10, -10,
-    -10, 0, 10, 10, 10, 10, 0, -10,
-    -10, 5, 5, 10, 10, 5, 5, -10,
-    -10, 0, 5, 10, 10, 5, 0, -10,
-    -10, 0, 0, 0, 0, 0, 0, -10,
-    -20, -10, -10, -10, -10, -10, -10, -20
-    ]
-rookstable = [
-    0, 0, 0, 5, 5, 0, 0, 0,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    5, 10, 10, 10, 10, 10, 10, 5,
-    0, 0, 0, 0, 0, 0, 0, 0
-    ]
-queenstable = [
-    -20, -10, -10, -5, -5, -10, -10, -20,
-    -10, 0, 0, 0, 0, 0, 0, -10,
-    -10, 0, 5, 5, 5, 5, 0, -10,
-    -5, 0, 5, 5, 5, 5, 0, -5,
-    0, 0, 5, 5, 5, 5, 0, -5,
-    -10, 5, 5, 5, 5, 5, 0, -10,
-    -10, 0, 5, 0, 0, 0, 0, -10,
-    -20, -10, -10, -5, -5, -10, -10, -20
-    ]
-kingstable = [
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    20, -30, -30, -40, -40, -30, -30, -20,
-    -10, -20, -20, -20, -20, -20, -20, -10,
-    20, 20, 0, 0, 0, 0, 20, 20,
-    20, 30, 10, 0, 0, 10, 30, 20
-]
+import chess
 
-#https://medium.com/dscvitpune/lets-create-a-chess-ai-8542a12afef
+def evaluate(board, player_is_white):
+    if board.is_checkmate():
+        return 9999 if board.turn else -9999
+    if board.is_stalemate() or board.is_insufficient_material() or board.is_fivefold_repetition():
+        return 0
 
-board = chess.Board()
-def evaluate(board, player):
-  if board.is_checkmate():
-      if board.turn:
-          return 9999
-      else:
-          return -9999
-  if board.is_stalemate():
-      return 0
-  if board.is_insufficient_material():
-      return 0
-  if board.is_fivefold_repetition(): #I added this
-      return 0
-  wp = len(board.pieces(chess.PAWN, chess.WHITE))
-  bp = len(board.pieces(chess.PAWN, chess.BLACK))
-  wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
-  bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
-  wb = len(board.pieces(chess.BISHOP, chess.WHITE))
-  bb = len(board.pieces(chess.BISHOP, chess.BLACK))
-  wr = len(board.pieces(chess.ROOK, chess.WHITE))
-  br = len(board.pieces(chess.ROOK, chess.BLACK))
-  wq = len(board.pieces(chess.QUEEN, chess.WHITE))
-  bq = len(board.pieces(chess.QUEEN, chess.BLACK))
-  material = 100 * (wp-bp) + 320 * (wn-bn) + 330 * (wb-bb) + 500 * (wr-br) + 900 * (wq-bq)
-  pawnsq = sum([pawntable[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
-  pawnsq = pawnsq + sum([-pawntable[chess.square_mirror(i)] for i in board.pieces(chess.PAWN, chess.BLACK)])
-  knightsq = sum([knightstable[i] for i in board.pieces(chess.KNIGHT, chess.WHITE)])
-  knightsq = knightsq + sum([-knightstable[chess.square_mirror(i)] for i in board.pieces(chess.KNIGHT, chess.BLACK)])
-  bishopsq = sum([bishopstable[i] for i in board.pieces(chess.BISHOP, chess.WHITE)])
-  bishopsq = bishopsq + sum([-bishopstable[chess.square_mirror(i)] for i in board.pieces(chess.BISHOP, chess.BLACK)])
-  rooksq = sum([rookstable[i] for i in board.pieces(chess.ROOK, chess.WHITE)]) 
-  rooksq = rooksq + sum([-rookstable[chess.square_mirror(i)] for i in board.pieces(chess.ROOK, chess.BLACK)])
-  queensq = sum([queenstable[i] for i in board.pieces(chess.QUEEN, chess.WHITE)]) 
-  queensq = queensq + sum([-queenstable[chess.square_mirror(i)] for i in board.pieces(chess.QUEEN, chess.BLACK)])
-  kingsq = sum([kingstable[i] for i in board.pieces(chess.KING, chess.WHITE)]) 
-  kingsq = kingsq + sum([-kingstable[chess.square_mirror(i)] for i in board.pieces(chess.KING, chess.BLACK)])
-  eval = material + pawnsq + knightsq + bishopsq + rooksq + queensq + kingsq
-  if player: #I modified this part
-    return eval  
-  else:
-    return -eval
+    piece_values = {
+        chess.PAWN: 100,
+        chess.KNIGHT: 320,
+        chess.BISHOP: 330,
+        chess.ROOK: 500,
+        chess.QUEEN: 900
+    }
 
+    player_color = chess.WHITE if player_is_white else chess.BLACK
+    opponent_color = not player_color
+
+    material = sum((len(board.pieces(piece, player_color)) - len(board.pieces(piece, opponent_color))) * value for piece, value in piece_values.items())
+
+    piece_square_tables = {
+        chess.PAWN: [100, 100, 100, 100, 100, 100, 100, 100,
+                     50,  50,  50,  50,  50,  50,  50,  50,
+                     10,  10,  20,  30,  30,  20,  10,  10,
+                     5,   5,   10,  25,  25,  10,  5,   5,
+                     0,   0,   0,   20,  20,  0,   0,   0,
+                     5,  -5,  -10,  0,   0,  -10, -5,   5,
+                     5,  10,  10, -20, -20,  10,  10,  5,
+                     0,   0,   0,   0,   0,   0,   0,   0],
+
+        chess.KNIGHT: [-50, -40, -30, -30, -30, -30, -40, -50,
+                        -40, -20,   0,   0,   0,   0, -20, -40,
+                        -30,   0,  10,  15,  15,  10,   0, -30,
+                        -30,   5,  15,  20,  20,  15,   5, -30,
+                        -30,   0,  15,  20,  20,  15,   0, -30,
+                        -30,   5,  10,  15,  15,  10,   5, -30,
+                        -40, -20,   0,   5,   5,   0, -20, -40,
+                        -50, -40, -20, -30, -30, -20, -40, -50],
+
+        chess.BISHOP: [-20, -10, -10, -10, -10, -10, -10, -20,
+                        -10,   0,   0,   0,   0,   0,   0, -10,
+                        -10,   0,   5,  10,  10,   5,   0, -10,
+                        -10,   5,   5,  10,  10,   5,   5, -10,
+                        -10,   0,  10,  10,  10,  10,   0, -10,
+                        -10,  10,  10,  10,  10,  10,  10, -10,
+                        -10,   5,   0,   0,   0,   0,   5, -10,
+                        -20, -10, -40, -10, -10, -40, -10, -20],
+
+        chess.ROOK: [0, 0, 0, 0, 0, 0, 0, 0,
+                     5, 10, 10, 10, 10, 10, 10, 5,
+                     -5, 0, 0, 0, 0, 0, 0, -5,
+                     -5, 0, 0, 0, 0, 0, 0, -5,
+                     -5, 0, 0, 0, 0, 0, 0, -5,
+                     -5, 0, 0, 0, 0, 0, 0, -5,
+                     -5, 0, 0, 0, 0, 0, 0, -5,
+                     0, 0, 0, 5, 5, 0, 0, 0],
+
+        chess.QUEEN: [-20, -10, -10, -5, -5, -10, -10, -20,
+                        -10,   0,   0,  0,  0,  0,  0, -10,
+                        -10,   0,   5,  5,  5,  5,  0, -10,
+                        -5,   0,   5,  5,  5,  5,  0,  -5,
+                         0,   0,   5,  5,  5,  5,  0,  -5,
+                        -10,   5,   5,  5,  5,  5,  0, -10,
+                        -10,   0,   5,  0,  0,  0,  0, -10,
+                        -20, -10, -10, -5, -5, -10, -10, -20],
+
+        chess.KING: [-30, -40, -40, -50, -50, -40, -40, -30,
+                      -30, -40, -40, -50, -50, -40, -40, -30,
+                      -30, -40, -40, -50, -50, -40, -40, -30,
+                      -30, -40, -40, -50, -50, -40, -40, -30,
+                      -20, -30, -30, -40, -40, -30, -30, -20,
+                      -10, -20, -20, -20, -20, -20, -20, -10,
+                       20,  20,   0,   0,   0,   0,  20,  20,
+                       20,  30,  10,   0,   0,  10,  30,  20]
+    }
+
+    piece_square_eval = sum(piece_square_tables[piece][square] for piece in piece_square_tables for square in board.pieces(piece, player_color))
+    piece_square_eval -= sum(piece_square_tables[piece][chess.square_mirror(square)] for piece in piece_square_tables for square in board.pieces(piece, opponent_color))
+
+    total_evaluation = material + piece_square_eval
+
+    return total_evaluation if player_is_white else -total_evaluation
