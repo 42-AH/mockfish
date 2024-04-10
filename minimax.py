6bd42b1f189ff74ac31f40be7c348f3a1709b0d1
42-AH <162044943+42-AH@users.noreply.github.com>
2024-04-09 19:33:42 -0600

    Update minimax.py

--- minimax.py
+++ minimax.py
@@ -1,3 +1,5 @@
+#This is all me
+
 import math
 
 import chess
@@ -7,34 +9,43 @@ from eval import evaluate
 board = chess.Board()
 
 
-def minimax(board, depth, is_maximizing):
+def minimax(board, depth, alpha, beta, is_maximizing):
   score = evaluate(board, is_maximizing) 
-  if depth == 0:
+  if depth == 0 or board.is_game_over():
       return score
   if is_maximizing:
       max_eval = float('-inf')
       for move in board.legal_moves:
           board.push(move)
-          eval = minimax(board, depth - 1, False)
+          eval = minimax(board, depth - 1, alpha, beta, False)
           board.pop()
           max_eval = max(max_eval, eval)
+          alpha = max(alpha, eval)
+          if beta <= alpha:
+              break
       return max_eval
   else:
       min_eval = float('inf')
       for move in board.legal_moves:
           board.push(move)
-          eval = minimax(board, depth - 1, True)
+          eval = minimax(board, depth - 1, alpha, beta, True)
           board.pop()
           min_eval = min(min_eval, eval)
+          beta = min(beta, eval)
+          if beta <= alpha:
+              break
       return min_eval
 def find_best_move(board, original_depth):
-    best_eval = -math.inf
-    best_move = None
-    for move in board.legal_moves:
-        board.push(move)
-        eval = minimax(board, original_depth, not board.turn)
-        board.pop()
-        if eval > best_eval:
-            best_eval = eval
-            best_move = move
-    return best_move
+  alpha = float("-inf")
+  beta = float("inf")
+  best_eval = -math.inf
+  best_move = None
+  for move in board.legal_moves:
+      board.push(move)
+      eval = minimax(board, original_depth, alpha, beta, not board.turn)
+      board.pop()
+      if eval > best_eval:
+          best_eval = eval
+          best_move = move
+      alpha = max(alpha, eval)
+  return best_move
