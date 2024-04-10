bfa80802cebb798a14e84c89d73f2bf99e586fb5
42-AH <162044943+42-AH@users.noreply.github.com>
2024-04-09 19:09:50 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import sys
+
+import chess
+
+from eval import evaluate
+from minimax import find_best_move
+
+
+def check():
+  if board.is_checkmate():
+    if board.turn:
+        print("Black won")
+        sys.exit()
+    else:
+        print("White won")
+        sys.exit()
+  if board.is_stalemate():
+    print("Draw")
+    sys.exit()
+  if board.is_insufficient_material():
+    print("Insufficient material")
+    sys.exit()
+  if board.is_fivefold_repetition():
+    print("Fivefold repitition")
+    sys.exit()
+board = chess.Board() 
+board.turn = chess.WHITE
+while not board.is_game_over():
+    check()
+    if board.turn:
+        move = find_best_move(board, 2)
+        print("WHITE")
+    else:
+        move = find_best_move(board, 1)
+        print("BLACK")
+    board.push(move)
+    print(board)
+    print("Score: " + str(evaluate(board, not board.turn)))
+    print(" ")
+check()
+
+  
