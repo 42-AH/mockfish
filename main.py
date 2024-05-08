b94cf5fbfcbc44a8607fa53fada18d3bad39db6f
42-AH <162044943+42-AH@users.noreply.github.com>
2024-05-08 07:31:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#pyinstaller --onefile main.py
+
+
 import sys
 
 import chess
@@ -5,7 +8,10 @@ import chess
 from eval import evaluate
 from minimax import find_best_move
 
-
+print("MOCKFISH")
+print("Copyright (C) 2023 42-AH")
+print("All rights reserved")
+print("")
 def check():
   if board.is_checkmate():
     if board.turn:
@@ -23,21 +29,48 @@ def check():
   if board.is_fivefold_repetition():
     print("Fivefold repitition")
     sys.exit()
-board = chess.Board() 
-board.turn = chess.WHITE
-while not board.is_game_over():
-    check()
-    if board.turn:
-        user_move = input("Move: ")
-        move = chess.Move.from_uci(user_move)
-        board.push(move)
-        print("WHITE")
-        print("Score: " + str(evaluate(board, False)))
+board = chess.Board()
+turn = "NONE"
+while True:
+    turn = input("WHITE OR BLACK: ").upper()
+    if turn in ["WHITE", "BLACK"]:
+        print("")
+        break
     else:
-        move = find_best_move(board, 3)
-        print("BLACK")
-        board.push(move)
-        print("Score: " + str(evaluate(board, False)))
-    print(board)
-    print(" ")
-check()
+        print("Invalid input. Please enter 'WHITE' or 'BLACK'.")
+
+depth = int(input("DEPTH: "))
+print("")
+if turn == "WHITE":
+    while not board.is_game_over():
+        check()
+        if board.turn:
+            print("WHITE TO MOVE")
+            user_move = input("Move: ")
+            move = chess.Move.from_uci(user_move)
+            board.push(move)
+            print("Score: " + str(evaluate(board, False)))
+        else:
+            print("BLACK TO MOVE")
+            move = find_best_move(board, depth)
+            board.push(move)
+            print("Score: " + str(evaluate(board, False)))
+        print(board)
+        print(" ")
+    check()
+else:
+    while not board.is_game_over():
+        check()
+        if board.turn:
+            print("WHITE TO MOVE")
+            move = find_best_move(board, depth)
+            board.push(move)
+            print("Score: " + str(evaluate(board, False)))
+        else:
+            print("BLACK TO MOVE")
+            user_move = input("Move: ")
+            move = chess.Move.from_uci(user_move)
+            board.push(move)
+            print("Score: " + str(evaluate(board, False)))
+        print(board)
+        print(" ")
