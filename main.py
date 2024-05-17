1aac6f163e1a91a3b88aefeec326addb944388fb
42-AH <162044943+42-AH@users.noreply.github.com>
2024-05-16 20:23:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -17,40 +17,12 @@ print("Copyright (C) 2023 42-AH")
 print("All rights reserved")
 
 
-def check(board):
-    if board.is_checkmate():
-        if board.turn:
-            print("Black won")
-            sys.exit()
-        else:
-            print("White won")
-            sys.exit()
-    if board.is_stalemate():
-        print("Draw")
-        sys.exit()
-    if board.is_insufficient_material():
-        print("Insufficient material")
-        sys.exit()
-    if board.is_fivefold_repetition():
-        print("Fivefold repetition")
-        sys.exit()
 
 
 def main():
     board = chess.Board()
-
-    while True:
-        player_color = input("Choose your color (WHITE/BLACK): ").upper()
-        if player_color in ["WHITE", "BLACK"]:
-            break
-        else:
-            print("Invalid input. Please enter 'WHITE' or 'BLACK'.")
-
-    depth = int(input("DEPTH: "))
-    print("")
-
     root = tk.Tk()
-    chess_ui = ChessUI(root, depth, player_color)
+    chess_ui = ChessUI(root)
     root.mainloop()
 
 
