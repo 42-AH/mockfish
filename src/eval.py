265cf8c1afda8b01305fbbb2c5e0c99ad6747f42
42-AH <162044943+42-AH@users.noreply.github.com>
2024-06-05 19:20:10 -0600

    Update eval.py

--- src/eval.py
+++ src/eval.py
@@ -1,78 +1,152 @@
-import math
 import chess
-from eval import evaluate
-import concurrent.futures
-import copy
 
-board = chess.Board()
 
-def move_ordering(board, move):
-    if board.gives_check(move):
-        return 10
-    if board.is_capture(move):
-        return 5
-    return 0
+def get_square(row, column):
+    return chess.square(column, 7 - row)
 
-def minimax(board, depth, alpha, beta, is_maximizing):
-    if depth == 0 or board.is_game_over():
-        score = evaluate(board, board.turn)
-        if score == 9999 or score == -9999:
-            score = score * (depth + 1)
-        return score
 
-    if is_maximizing:
-        max_eval = float('-inf')
-        for move in board.legal_moves:
-            board.push(move)
-            eval = minimax(board, depth - 1, alpha, beta, board.turn)
-            board.pop()
-            max_eval = max(max_eval, eval)
-            alpha = max(alpha, eval)
-            if beta <= alpha:
-                break
-        return max_eval
+def evaluate(board, maximizing):
+    player_color = chess.WHITE if board.turn else chess.BLACK
+    opponent_color = not player_color
+
+    if board.is_checkmate():
+        outcome = board.outcome()
+        if outcome:
+            return -9999 if board.turn else 9999
+    if board.is_stalemate() or board.is_insufficient_material() or board.is_fivefold_repetition() or board.can_claim_threefold_repetition():
+        return 0
+
+    piece_values = {
+        chess.PAWN: 100,
+        chess.KNIGHT: 320,
+        chess.BISHOP: 330,
+        chess.ROOK: 500,
+        chess.QUEEN: 900,
+        chess.KING: 9999
+    }
+
+    material = sum(
+        (len(board.pieces(piece, player_color)) - len(board.pieces(piece, opponent_color))) * value for piece, value in
+        piece_values.items())
+
+    total = sum(
+        (len(board.pieces(piece, player_color)) + len(board.pieces(piece, opponent_color))) * value for piece, value in
+        piece_values.items())
+
+    piece_square_tables = {
+        chess.PAWN: [0,   0,   0,   0,   0,   0,   0,   0,
+                    0,   0,   0, -40, -40,   0,   0,   0,
+                    1,   2,   3, -10, -10,   3,   2,   1,
+                    2,   4,   6,   8,   8,   6,   4,   2,
+                    3,   6,   9,  12,  12,   9,   6,   3,
+                    4,   8,  12,  16,  16,  12,   8,   4,
+                    5,  10,  15,  20,  20,  15,  10,   5,
+                    0,   0,   0,   0,   0,   0,   0,   0],
+
+        chess.KNIGHT: [-10, -10, -10, -10, -10, -10, -10, -10,
+                        -10,   0,   0,   0,   0,   0,   0, -10,
+                        -10,   0,   5,   5,   5,   5,   0, -10,
+                        -10,   0,   5,  10,  10,   5,   0, -10,
+                        -10,   0,   5,  10,  10,   5,   0, -10,
+                        -10,   0,   5,   5,   5,   5,   0, -10,
+                        -10,   0,   0,   0,   0,   0,   0, -10,
+                        -10, -30, -10, -10, -10, -10, -30, -10],
+
+        chess.BISHOP: [-10, -10, -10, -10, -10, -10, -10, -10,
+                        -10,   0,   0,   0,   0,   0,   0, -10,
+                        -10,   0,   5,   5,   5,   5,   0, -10,
+                        -10,   0,   5,  10,  10,   5,   0, -10,
+                        -10,   0,   5,  10,  10,   5,   0, -10,
+                        -10,   0,   5,   5,   5,   5,   0, -10,
+                        -10,   0,   0,   0,   0,   0,   0, -10,
+                        -10, -10, -20, -10, -10, -20, -10, -10],
+
+        chess.ROOK: [0, -75, 0, 5, 5, 0, -75, 0,
+                     -5, 0, 0, 0, 0, 0, 0, -5,
+                     -5, 0, 0, 0, 0, 0, 0, -5,
+                     -5, 0, 0, 0, 0, 0, 0, -5,
+                     -5, 0, 0, 0, 0, 0, 0, -5,
+                     -5, 0, 0, 0, 0, 0, 0, -5,
+                     5, 10, 10, 10, 10, 10, 10, 5,
+                     0, 0, 0, 0, 0, 0, 0, 0, ],
+
+        chess.QUEEN: [-20,-10,-10, -5, -5,-10,-10,-20,
+                        -10,  0,  0,  0,  0,  0,  0,-10,
+                        -10,  0,  5,  5,  5,  5,  0,-10,
+                         -5,  0,  5,  5,  5,  5,  0, -5,
+                          0,  0,  5,  5,  5,  5,  0, -5,
+                        -10,  5,  5,  5,  5,  5,  0,-10,
+                        -10,  0,  5,  0,  0,  0,  0,-10,
+                        -20,-10,-10, -5, -5,-10,-10,-20],
+
+        chess.KING: [  0,  20,  40, -20,   0, -20,  40,  20,
+                    -20, -20, -20, -20, -20, -20, -20, -20,
+                    -40, -40, -40, -40, -40, -40, -40, -40,
+                    -40, -40, -40, -40, -40, -40, -40, -40,
+                    -40, -40, -40, -40, -40, -40, -40, -40,
+                    -40, -40, -40, -40, -40, -40, -40, -40,
+                    -40, -40, -40, -40, -40, -40, -40, -40,
+                    -40, -40, -40, -40, -40, -40, -40, -40]
+    }
+
+    king_endgame = [0, 10, 20, 30, 30, 20, 10, 0,
+                    10, 20, 30, 40, 40, 30, 20, 10,
+                    20, 30, 40, 50, 50, 40, 30, 20,
+                    30, 40, 50, 60, 60, 50, 40, 30,
+                    30, 40, 50, 60, 60, 50, 40, 30,
+                    20, 30, 40, 50, 50, 40, 30, 20,
+                    10, 20, 30, 40, 40, 30, 20, 10,
+                    0, 10, 20, 30, 30, 20, 10, 0]
+
+    rook_endgame = [0, 0, 0, 5, 5, 0, 0, 0,
+                     -5, 0, 0, 0, 0, 0, 0, -5,
+                     -5, 0, 0, 0, 0, 0, 0, -5,
+                     -5, 0, 0, 0, 0, 0, 0, -5,
+                     -5, 0, 0, 0, 0, 0, 0, -5,
+                     -5, 0, 0, 0, 0, 0, 0, -5,
+                     5, 10, 10, 10, 10, 10, 10, 5,
+                     0, 0, 0, 0, 0, 0, 0, 0, ]
+
+    if total < 2000:
+        piece_square_tables[chess.KING] = king_endgame
+        piece_square_tables[chess.ROOK] = rook_endgame
+    if player_color == chess.WHITE:
+        piece_square_eval = sum(piece_square_tables[piece][square] for piece in piece_square_tables for square in
+                                board.pieces(piece, player_color))
+        piece_square_eval -= sum(
+            piece_square_tables[piece][chess.square_mirror(square)] for piece in piece_square_tables for square in
+            board.pieces(piece, opponent_color))
     else:
-        min_eval = float('inf')
-        for move in board.legal_moves:
-            board.push(move)
-            eval = minimax(board, depth - 1, alpha, beta, board.turn)
-            board.pop()
-            min_eval = min(min_eval, eval)
-            beta = min(beta, eval)
-            if beta <= alpha:
-                break
-        return min_eval
+        piece_square_eval = sum(
+            piece_square_tables[piece][chess.square_mirror(square)] for piece in piece_square_tables for square in
+            board.pieces(piece, player_color))
+        piece_square_eval -= sum(piece_square_tables[piece][square] for piece in piece_square_tables for square in
+                                 board.pieces(piece, opponent_color))
+    total_evaluation = material + piece_square_eval
 
-def evaluate_move(board, move, original_depth, alpha, beta):
-    board_copy = copy.deepcopy(board)
-    board_copy.push(move)
-    eval = minimax(board_copy, original_depth - 1, alpha, beta, board_copy.turn)
-    return (move, eval)
+    doubled_penalty = 30
+    for row in range(8):
+        for column in range(8):
+            square = get_square(row, column)
+            piece = board.piece_at(square)
 
-def find_best_move(board, original_depth):
-    alpha = float("-inf")
-    beta = float("inf")
-    best_eval = -math.inf
-    worst_eval = math.inf
-    best_move = None
+            if piece and piece.symbol() == "P":
+                if row + 1 < 8:
+                    piece_above = board.piece_at(get_square(row + 1, column))
+                    if piece_above and piece_above.symbol() == "P":
+                        total_evaluation -= doubled_penalty
+                if row - 1 >= 0:
+                    piece_below = board.piece_at(get_square(row - 1, column))
+                    if piece_below and piece_below.symbol() == "P":
+                        total_evaluation -= doubled_penalty
+            elif piece and piece.symbol() == "p":
+                if row + 1 < 8:
+                    piece_above = board.piece_at(get_square(row + 1, column))
+                    if piece_above and piece_above.symbol() == "p":
+                        total_evaluation += doubled_penalty
+                if row - 1 >= 0:
+                    piece_below = board.piece_at(get_square(row - 1, column))
+                    if piece_below and piece_below.symbol() == "p":
+                        total_evaluation += doubled_penalty
 
-    moves = list(sorted(board.legal_moves, key=lambda move: move_ordering(board, move), reverse=True))
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [executor.submit(evaluate_move, board, move, original_depth, alpha, beta) for move in moves]
-        for future in concurrent.futures.as_completed(futures):
-            move, eval = future.result()
-            if board.turn:
-                if eval > best_eval:
-                    best_eval = eval
-                    best_move = move
-                alpha = max(alpha, eval)
-            else:
-                if eval < worst_eval:
-                    worst_eval = eval
-                    best_move = move
-                beta = min(beta, eval)
-            if beta <= alpha:
-                break
-
-    return best_move
+    return total_evaluation if maximizing else -total_evaluation
