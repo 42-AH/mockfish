1474cc5b8e73610340ce5dbc86b06f7dfaa18e53
42-AH <162044943+42-AH@users.noreply.github.com>
2024-10-15 19:49:22 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,23 +1,63 @@
 import sys
-import tkinter as tk
-import chess
-from chess_ui import ChessUI
-import tkinter as tk
-from PIL import Image, ImageTk
-import os
 import chess
 from minimax import find_best_move
-import sys
-import math
-import time
 import chess.polyglot
 
+def print_board(board, color):
+    if color == "b":
+        print(board.transform(chess.flip_vertical))
+    else:
+        print(board)
 
 def main():
     board = chess.Board()
-    root = tk.Tk()
-    chess_ui = ChessUI(root)
-    root.mainloop()
+
+    while True:
+        try:
+            depth = int(input("Depth (1-7): "))
+            if 1 <= depth <= 7:
+                break
+            else:
+                print("Please enter a depth between 1 and 7.")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+    while True:
+        color = input("Color (w/b): ").lower()
+        if color in ["w", "b"]:
+            break
+        else:
+            print("Invalid input. Please enter 'w' for white or 'b' for black.")
+
+    if color == "b":
+        ai_move = find_best_move(board, depth)
+        board.push(ai_move)
+        print("AI move:")
+        print_board(board, color)
+        print("\n")
+
+    while not board.is_game_over():
+        print_board(board, color)
+
+        while True:
+            user_move = input("Your move (in UCI format): ").lower()
+            try:
+                board.push_uci(user_move)
+                break
+            except ValueError:
+                print("Invalid move. Please try again.")
+
+        if board.is_game_over():
+            break
+
+        ai_move = find_best_move(board, depth)
+        board.push(ai_move)
+        print("AI move:")
+        print_board(board, color)
+        print("\n")
+
+    print("Game over!")
+    print(board.result())
 
 if __name__ == "__main__":
     main()
