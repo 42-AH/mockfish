a62fdd648b2825be110c07b46e55ed62b2451014
42-AH <162044943+42-AH@users.noreply.github.com>
2024-11-04 16:26:02 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,63 +1,26 @@
 import sys
+import tkinter as tk
+import chess
+from chess_ui import ChessUI
+import tkinter as tk
+from PIL import Image, ImageTk
+import os
 import chess
 from minimax import find_best_move
+import sys
+import math
+import time
 import chess.polyglot
 
-def print_board(board, color):
-    if color == "b":
-        print(board.transform(chess.flip_vertical))
-    else:
-        print(board)
 
 def main():
     board = chess.Board()
-
-    while True:
-        try:
-            depth = int(input("Depth (1-6): "))
-            if 1 <= depth <= 6:
-                break
-            else:
-                print("Please enter a depth between 1 and 6.")
-        except ValueError:
-            print("Invalid input. Please enter an integer.")
-
-    while True:
-        color = input("Color (w/b): ").lower()
-        if color in ["w", "b"]:
-            break
-        else:
-            print("Invalid input. Please enter 'w' for white or 'b' for black.")
-
-    if color == "b":
-        ai_move = find_best_move(board, depth)
-        board.push(ai_move)
-        print("AI move:" + str(ai_move))
-        print_board(board, color)
-        print("\n")
-
-    while not board.is_game_over():
-        while True:
-            user_move = input("Your move (in UCI format): ").lower()
-            try:
-                board.push_uci(user_move)
-                print_board(board, color)
-                print("\n")
-                break
-            except ValueError:
-                print("Invalid move. Please try again.")
-
-        if board.is_game_over():
-            break
-
-        ai_move = find_best_move(board, depth)
-        board.push(ai_move)
-        print("AI move:" + str(ai_move))
-        print_board(board, color)
-        print("\n")
-
-    print("Game over!")
-    print(board.result())
+    root = tk.Tk()
+    chess_ui = ChessUI(root)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
+
+# pyinstaller main.py --noconfirm --onefile --windowed --add-data "C:/Users/dougl/PycharmProjects/Mockfish/assets;assets/"
+
